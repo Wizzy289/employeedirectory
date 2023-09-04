e7da96f4b603de62b7321436a45a2dcd33fb028e
Wizzy <wizdomiteobaro@gmail.com>
2023-09-04 19:19:51 +0100

    message

--- src/Components/EmployeeTable/EmployeeTable.js
+++ src/Components/EmployeeTable/EmployeeTable.js
@@ -1,50 +1,31 @@
-import React from 'react'
-import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
-import employees from './Employees.json'
+import React from 'react';
+import './EmployeeTable.css'
 
-export default function EmployeeTable({ employees, onEmployeeClick }) {
+const EmployeeTable = ({ employees, onEmployeeClick }) => {
     return (
-        <div className='table'>
-            <MDBTable align='middle'>
-                <MDBTableHead>
-                    <tr>
-                        <th scope='col'>Name</th>
-                        <th scope='col'>Job Title</th>
-                        <th scope='col'>Department</th>
-
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Job Title</th>
+                    <th>Department</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {employees.map((employee) => (
+                    <tr key={employee.id}>
+                        <td>{employee.name}</td>
+                        <td>{employee.job}</td>
+                        <td>{employee.department}</td>
+                        <td style={{textAlign:'center'}}>
+                            <button onClick={() => onEmployeeClick(employee)}>View Details</button>
+                        </td>
                     </tr>
-                </MDBTableHead>
-                <MDBTableBody>
-                    {employees.map((employee) => (
-                        <tr key={employee.id}>
-                            <td>
-                                <div className='d-flex align-items-center'>
+                ))}
+            </tbody>
+        </table>
+    );
+};
 
-                                    <div className='ms-3'>
-                                        <p className='fw-bold mb-1'>{employee.name}</p>
-                                        <p className='text-muted mb-0'>{employee.email}</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <p className='fw-normal mb-1'>{employee.job}</p>
-
-                            </td>
-                            <td>
-                                <p className='text-muted mb-0'>{employee.department}</p>
-                            </td>
-                            
-                            <td>
-                                <MDBBtn color='dark' rounded size='sm' onClick={() => onEmployeeClick(employee)}>
-                                    View Details
-                                </MDBBtn>
-                            </td>
-                        </tr>
-
-                    ))}
-
-                </MDBTableBody>
-            </MDBTable>
-        </div>
-    )
-}
+export default EmployeeTable;
